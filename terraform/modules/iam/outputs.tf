6f41b2e95c8c8522f43c8631517752817a29e5f1
ronbarzilay0 <ron.barzilay.rb@gmail.com>
2026-06-02 21:53:14 +0300

    Update outputs.tf

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,1 +1,12 @@
+output "ec2_instance_profile_name" {
+  value = aws_iam_instance_profile.ec2.name
+}
 
+output "ec2_role_arn" {
+  value = aws_iam_role.ec2.arn
+}
+
+output "github_actions_role_arn" {
+  description = "ARN for GitHub Actions to assume via OIDC"
+  value       = aws_iam_role.github_actions.arn
+}
