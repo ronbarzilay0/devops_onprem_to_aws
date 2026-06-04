f42abe95dd88f80beacdc43ed59d65ef509aa76a
ronbarzilay0 <ron.barzilay.rb@gmail.com>
2026-06-04 14:38:48 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,12 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region                      = var.aws_region
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
 
   default_tags {
     tags = {
