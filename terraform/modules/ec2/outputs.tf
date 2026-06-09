be2772136b8d5b91feb935973bd81f20e5a042b4
ronbarzilay0 <ron.barzilay.rb@gmail.com>
2026-06-09 17:36:55 +0300

    Update outputs.tf

--- terraform/modules/ec2/outputs.tf
+++ terraform/modules/ec2/outputs.tf
@@ -3,11 +3,6 @@ output "asg_name" {
   value       = aws_autoscaling_group.main.name
 }
 
-output "ec2_security_group_id" {
-  description = "ID of the EC2 security group — passed to the rds module to allow DB access from EC2 only"
-  value       = aws_security_group.ec2.id
-}
-
 output "launch_template_id" {
   description = "ID of the launch template"
   value       = aws_launch_template.main.id
