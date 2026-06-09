52e6d2dc6ac0d9c85bea048624dea128ead42888
ronbarzilay0 <ron.barzilay.rb@gmail.com>
2026-06-09 17:45:38 +0300

    Update variables.tf

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -24,6 +24,11 @@ variable "ami_id" {
   type        = string
 }
 
+variable "ec2_security_group_id" {
+  description = "The ID of the EC2 security group"
+  type        = string
+}
+
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
