48a3240504f19393ebf7d400ed74cdd0fdc52963
ronbarzilay0 <ron.barzilay.rb@gmail.com>
2026-06-09 16:54:45 +0300

    Update variables.tf

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -7,3 +7,14 @@ variable "environment" {
   description = "Deployment environment (e.g. prod, staging, dev)"
   type        = string
 }
+
+variable "vpc_id" {
+  description = "VPC ID for security groups"
+  type        = string
+}
+
+variable "app_port" {
+  description = "Port the microservices listen on"
+  type        = number
+  default     = 8080
+}
