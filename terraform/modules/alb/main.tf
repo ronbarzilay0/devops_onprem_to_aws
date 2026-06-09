35dd76e6e336c06224a045ea936ede9a34b2adf8
ronbarzilay0 <ron.barzilay.rb@gmail.com>
2026-06-09 16:58:22 +0300

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -8,46 +8,7 @@ terraform {
 
   required_version = ">= 1.5.0"
 }
-# ─────────────────────────────────────────
-# Security Group for ALB
-# Allows inbound HTTP/HTTPS from anywhere (public-facing)
-# EC2 security group will only allow traffic from this SG
-# ─────────────────────────────────────────
-resource "aws_security_group" "alb" {
-  name        = "${var.project_name}-${var.environment}-alb-sg"
-  description = "Security group for the Application Load Balancer"
-  vpc_id      = var.vpc_id
 
-  # allow traffic into the security group only through http and http
-  ingress {
-    description = "Allow HTTP from internet"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow HTTPS from internet"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # allow to send traffic outward to anywhere from any port 
-  egress {
-    description = "Allow all outbound to reach EC2 instances"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project_name}-${var.environment}-alb-sg"
-  }
-}
 
 # ─────────────────────────────────────────
 # Application Load Balancer
