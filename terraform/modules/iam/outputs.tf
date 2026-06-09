62c745026fdd0052b26b269174e842d6d96eb075
ronbarzilay0 <ron.barzilay.rb@gmail.com>
2026-06-09 16:55:02 +0300

    Update outputs.tf

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -12,3 +12,18 @@ output "ec2_role_name" {
   description = "Name of the EC2 IAM role"
   value       = aws_iam_role.ec2.name
 }
+
+output "alb_security_group_id" {
+  description = "Security group ID for the ALB"
+  value       = aws_security_group.alb.id
+}
+
+output "ec2_security_group_id" {
+  description = "Security group ID for EC2 instances"
+  value       = aws_security_group.ec2.id
+}
+
+output "rds_security_group_id" {
+  description = "Security group ID for RDS"
+  value       = aws_security_group.rds.id
+}
